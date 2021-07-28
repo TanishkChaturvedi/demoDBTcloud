56922abd504976b6fad9e9b6da911c5252d8ee89
tanishk.chaturvedi@hashmapinc.com <tanishk.chaturvedi@hashmapinc.com>
2021-07-28 05:13:49 +0000

    yml file

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,16 @@
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from raw.jaffle_shop.customers
+
+),
+
+final as (
+    select * from customers
+)
+
+select * from final
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,16 @@
+with amount as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount / 100 as amount,
+        created as created_at
+    from raw.STRIPE.payment
+),
+
+final as (
+    select * from amount
+)
+
+select * from final
