eab37e70be60547fb4f4118bb565a18f1f1ca6f3
tanishk.chaturvedi@hashmapinc.com <tanishk.chaturvedi@hashmapinc.com>
2021-07-29 06:16:33 +0000

    configured tests, sources, models

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -6,7 +6,7 @@ with amount as (
         status,
         amount / 100 as amount,
         created as created_at
-    from {{ source('stripe', 'payment') }}
+    from {{source('stripe','payment')}}
 ),
 
 final as (
